32ee9bae73ad9bfd1e92806c3307bceddbc952b4
liuying <liuying@baibu.la>
2019-12-12 20:56:17 +0800

    目录初始化

---
+++ Sur/ProLbyN6g.dpr
@@ -0,0 +1,14 @@
+program ProLbyN6g;
+
+uses
+  Forms,
+  UfrmMain in 'UfrmMain.pas' {frmMain},
+  UCommFunction in 'UCommFunction.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
